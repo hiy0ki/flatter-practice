ee4f7852bbdeb23360af56b12f02e3a0b115717a
hiyoki <maha.tree.21@gmail.com>
2019-10-06 17:04:13 +0900

    change color

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Startup name Generator',
       theme: ThemeData(
-        primaryColor: Colors.green,
+        primaryColor: Colors.red,
       ),
       home: RandomWords(),
     );
